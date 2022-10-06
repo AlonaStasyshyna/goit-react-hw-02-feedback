86a3421ac96f9223ed1bfde58974e0d3458b2d96
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-07 00:23:20 +0300

    Add all tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,32 @@ export const App = () => {
     </div>
   );
 };
+  }
+
+  countTotalFeedback = ({good, neutral, bad}) => (good + neutral + bad)
+
+  countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+    if (!good) {
+      return 0;
+    }
+    
+    return Math.round(good / (good + neutral + bad) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback}/>
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback({ good, neutral, bad })
+            ? <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback({ good, neutral, bad })} positivePercentage={this.countPositiveFeedbackPercentage({ good, neutral, bad })} />
+            : <Notification message="There is no feedback" />}
+        </Section>
+      </>
+    )}
+};
